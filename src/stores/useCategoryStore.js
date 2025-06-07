b734c1a07853f99dd82cd5f313206bfc0597ea6d
Moreene <moreenewu@gmail.com>
2025-06-07 16:18:53 +0800

    fix: 修改圖片

--- src/stores/useCategoryStore.js
+++ src/stores/useCategoryStore.js
@@ -1,6 +1,6 @@
 export const useCategoryStore = defineStore('category', () => {
   const categories = ref([
-    { name: '主廚推薦', path: 'recommendation', imageUrl: 'https://www.thaij.com/Public/Admin/202111175018.jpg' },
+    { name: '主廚推薦', path: 'recommendation', imageUrl: 'https://images.deliveryhero.io/image/fd-tw/LH/z5gv-listing.jpg' },
     { name: '開胃涼拌', path: 'salad', imageUrl: 'https://images.unsplash.com/photo-1650394248408-4434b41630bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=950&q=80' },
     { name: '大口吃肉', path: 'meat', imageUrl: 'https://images.unsplash.com/photo-1596797038530-2c107229654b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmVkJTIwY3Vycnl8ZW58MHx8MHx8fDA%3D' },
     { name: '大口吃菜', path: 'vegetable', imageUrl: 'https://www.thermos.com.tw/Upload/RECICONT/20/00000020.jpg' },
